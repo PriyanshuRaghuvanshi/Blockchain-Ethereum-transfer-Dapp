b6ba372fa73fd858818d0553a773bdcbe29e42aa
PriyanshuRaghuvanshi <priyanshuraghuvanshi18@gmail.com>
2022-02-16 18:01:16 +0530

    change2

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -47,7 +47,7 @@ const handleSubmit=(e)=>{
         <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
               <div className="flex flex-1 justify-start flex-col mf:mr-10">
                   <h1 className="text-3xl sm:text:5xl text-white text-gradient py-1">
-                      no Crypto <br/> across the world
+                      Send Crypto <br/> across the world
                   </h1>
                      <p className="text-left mt-5 text-white font-light md:w-9/12 text-base">
                          Explore the crypto world . Buy and sell cruptocurrencies easily on Krypt.
